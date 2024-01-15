68bffa95f641022fe7f3a16e7902a0a800a14e6d
Brian Kissell <57643247+BrianKissell@users.noreply.github.com>
2024-01-15 08:27:14 -0600

    I created the utils file, and then I created the adjust_file_path_to_current_machine function.

---
+++ R/utils.R
@@ -0,0 +1,40 @@
+#' adjust_file_path_to_current_machine
+#' This code converts any path so that it works on the current computerfor the user.
+#'
+#' @param file_path file_path
+#'
+#' @return goal_file_path
+#' @export
+#'
+adjust_file_path_to_current_machine <- function(file_path){
+
+  # Obtain the working directory from the user's computer.
+  local_working_directory <- getwd()
+
+  # Split the working directory into multiple parts to make it easier to work with.
+  parts_of_wd <- local_working_directory %>%
+    strsplit(.Platform$file.sep) %>%
+    unlist()
+
+  # Split the file_path into multiple parts to make it easier to work with.
+  parts_of_file_path <- {{file_path}} %>%
+    strsplit(.Platform$file.sep) %>%
+    unlist()
+
+  # Check if the first element of the working directory is a drive
+  if(parts_of_wd[[1]] == parts_of_file_path[[1]] & parts_of_wd[[1]] == parts_of_file_path[[1]]){
+
+    # Switch the computer name
+    parts_of_file_path[[3]] <- parts_of_wd[[3]]
+
+    # Recreate the file path
+    # goal_file_path <- paste0(parts_of_file_path, collapse = "/")
+    goal_file_path <- file.path(parts_of_file_path, collapse = "/")
+    return(goal_file_path)
+
+  } else {
+
+    # Otherwise, throw an error
+    stop("The program is unable to detect the files needed to adjust the file path.")
+  }
+}
