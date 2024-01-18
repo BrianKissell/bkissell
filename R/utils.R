7e9c853d1ebab5eecf636338c66e6eb99c7b8246
Brian Kissell <57643247+BrianKissell@users.noreply.github.com>
2024-01-18 17:43:27 -0600

    Created the full global variables function

--- R/utils.R
+++ R/utils.R
@@ -972,3 +972,107 @@ combine_file_string_with_time <- function(file_string){
   # Return the new string
   return(combined_string)
 }
+
+
+
+
+
+
+#' FULL_global_coding
+#'
+#' @param man_wd man_wd
+#' @param file_part__sm_raw_folder file_part__sm_raw_folder
+#' @param global_coding_file_path global_coding_file_path
+#' @param backup_for_combined_data_folder_path backup_for_combined_data_folder_path
+#' @param backup_for_editable_data_folder_path backup_for_editable_data_folder_path
+#' @param backup_for_raw_data_folder_path backup_for_raw_data_folder_path
+#' @param backup_for_data_to_add_folder_path backup_for_data_to_add_folder_path
+#'
+#' @return global_coding_to_save
+#' @export
+#'
+FULL_global_coding <- function(
+    man_wd = NULL,
+    file_part__sm_raw_folder = "Qualitative Coding/Version 2/global_variables/raw_data",
+    global_coding_file_path = "Qualitative Coding/Version 2/global_variables/Global_Coding_USE_TO_EDIT_RESPONSES.xlsx",
+    backup_for_combined_data_folder_path = "Qualitative Coding/Version 2/global_variables/backup_files/backup_for_combined_data",
+    backup_for_editable_data_folder_path = "Qualitative Coding/Version 2/global_variables/backup_files/backup_for_editable_data",
+    backup_for_raw_data_folder_path = "Qualitative Coding/Version 2/global_variables/backup_files/backup_for_raw_data",
+    backup_for_data_to_add_folder_path = "Qualitative Coding/Version 2/global_variables/backup_files/backup_for_data_to_add"
+) {
+
+  # Read in the data from survey monkey
+  survey_monkey_data <- bkissell::read_survey_monkey_data(
+    man_wd = man_wd,
+    file_part__sm_raw_folder = file_part__sm_raw_folder
+  )[[1]]
+
+  # Convert particular NAs to 0s, and other responses to 1s
+  survey_monkey_data <- survey_monkey_data %>%
+    mutate(
+      across(
+        c(starts_with("animals__"),
+          starts_with("mc_"),
+          starts_with("sc_"),
+          starts_with("bc_"),
+          starts_with("cel_"),
+          starts_with("premium__")
+        ),
+        ~{ifelse(is.na(.x), 0, 1)})
+    )
+
+  # Adjust organization of the data
+  survey_monkey_data <- survey_monkey_data %>%
+    dplyr::select(-c("collector_id", "email_address", "first_name", "last_name", "custom_data_1"))
+
+  # Create id so it with a letter so it is read correctly in excel
+  survey_monkey_data$respondent_id <- paste0("id_", survey_monkey_data$respondent_id)
+
+  #### Update Global Variables with Editable - Survey Monkey
+
+  global_coding_file_path <- {{global_coding_file_path}}
+
+  # Read in the editable file
+  global_coding_for_editing <- readxl::read_excel(global_coding_file_path)
+
+  # Remove NAs from the respondent id column
+  global_coding_for_editing <- global_coding_for_editing %>% dplyr::filter(!is.na(respondent_id))
+
+  # For the add file, remove the columns from the survey monkey data that already exist in the editable file
+  global_coding_to_add <- survey_monkey_data %>%
+    dplyr::filter(!(.data[["respondent_id"]] %in% global_coding_for_editing$respondent_id))
+
+  # Decide which files to use in differing circumstances
+  if(nrow(global_coding_to_add) == 0 & nrow(global_coding_for_editing) == 0) {
+    # If nothing exists, default to the editable file
+    global_coding_to_save <- global_coding_for_editing
+  } else if(nrow(global_coding_for_editing) == 0 & nrow(global_coding_to_add) > 0) {
+    # If only data exists in the data to add, use that
+    global_coding_to_save <- global_coding_to_add
+  } else if (nrow(global_coding_for_editing) > 0 & nrow(global_coding_to_add) == 0) {
+    # If data only exists in the editable data, use that
+    global_coding_to_save <- global_coding_for_editing
+  } else if (nrow(global_coding_for_editing) > 0 & nrow(global_coding_to_add) > 0) {
+    # Otherwise, add the two data sets together
+    global_coding_to_save <- global_coding_for_editing %>%
+      rbind(global_coding_to_add)
+  }
+
+  # Organize the data by end date
+  global_coding_to_save <- global_coding_to_save %>% arrange(end_date)
+
+  # Write all of the files
+  backup_for_combined_data_file_path <- paste0(backup_for_combined_data_folder_path, "/combined.csv") %>% bkissell::combine_file_string_with_time()
+  backup_for_editable_data_file_path <- paste0(backup_for_editable_data_folder_path, "/editable.csv") %>% bkissell::combine_file_string_with_time()
+  backup_for_raw_data_file_path <- paste0(backup_for_raw_data_folder_path, "/raw.csv") %>% bkissell::combine_file_string_with_time()
+  backup_for_data_to_add_file_path <- paste0(backup_for_data_to_add_folder_path, "/to_add.csv") %>% bkissell::combine_file_string_with_time()
+  readr::write_csv(global_coding_for_editing, backup_for_editable_data_file_path)
+  readr::write_csv(survey_monkey_data, backup_for_raw_data_file_path)
+  readr::write_csv(global_coding_to_add, backup_for_data_to_add_file_path)
+  readr::write_csv(global_coding_to_save, backup_for_combined_data_file_path)
+  readr::write_csv(global_coding_to_save, global_coding_file_path)
+  writexl::write_xlsx(global_coding_to_save, global_coding_file_path)
+
+  # Return the object
+  return(global_coding_to_save)
+}
